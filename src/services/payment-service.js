835d9ea257c130c03d53f78bad79006e928ad741
Chandan Tiwari <chandan.kr.tiwarii@gmail.com>
2026-09-10 10:23:16 +0000

    implemented payment simulation

--- src/services/payment-service.js
+++ src/services/payment-service.js
@@ -1,5 +1,8 @@
+const axios = require('axios');
 const PaymentRepository = require('../repositories/payment-repository');
 const { Enums } = require('../utils/common');
+const { Razorpay } = require('../utils/helpers/razorpay');
+const crypto = require('crypto');
 
 class PaymentService {
     constructor() {
@@ -19,7 +22,6 @@ class PaymentService {
         }
 
         const bookingResponse = await response.json();
-
         const booking = bookingResponse.data;
 
         // Check whether booking is eligible for payment
@@ -35,14 +37,68 @@ class PaymentService {
             throw new Error('Payment already exists for this booking');
         }
 
-        // Create payment
+        const razorpayOrder = await Razorpay.orders.create({
+            amount: booking.totalCost * 100,
+            currency: 'INR',
+            receipt: `booking_${booking.id}`
+        });
+
         const payment = await this.paymentRepository.create({
             bookingId: booking.id,
             userId: booking.userId,
             amount: booking.totalCost,
             status: Enums.PAYMENT_STATUS.INITIATED,
-            provider: 'RAZORPAY'
+            provider: 'RAZORPAY',
+            providerOrderId: razorpayOrder.id
         });
+
+        return {
+            payment,
+            razorpayOrder
+        };
+    }
+
+    async verifyPayment(data) {
+        const {
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature
+        } = data;
+
+        const payment =
+            await this.paymentRepository.findByProviderOrderId(
+                razorpay_order_id
+            );
+
+        if (!payment) {
+            throw new Error('Payment not found');
+        }
+
+        const body =
+            razorpay_order_id + '|' + razorpay_payment_id;
+
+        const expectedSignature = crypto
+            .createHmac(
+                'sha256',
+                process.env.RAZORPAY_KEY_SECRET
+            )
+            .update(body)
+            .digest('hex');
+
+        if (expectedSignature !== razorpay_signature) {
+            throw new Error('Invalid payment signature');
+        }
+
+        // 1. Mark payment as successful
+        await payment.update({
+            providerPaymentId: razorpay_payment_id,
+            status: Enums.PAYMENT_STATUS.SUCCESS
+        });
+
+        // 2. Confirm booking
+        await axios.patch(
+            `${process.env.AEROBOOK_BOOKING_SERVICE}/api/v1/bookings/${payment.bookingId}/confirm`
+        );
 
         return payment;
     }
